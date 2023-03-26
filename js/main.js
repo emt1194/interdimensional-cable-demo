a65ad23c5a8f2427471515d5a12e6eaf90ed512a
emt1194 <129013824+emt1194@users.noreply.github.com>
2023-03-26 16:22:50 -0700

    1st
    interdimensional cable, but boiling acid. buggy, needs fixing

---
+++ js/main.js
@@ -0,0 +1,154 @@
+var tv = null;
+
+var clip_list = [
+    "content/acid/boiling-acid-1.webm"
+    , "content/acid/boiling-acid-2.mp4"
+    , "content/acid/boiling-acid-3.mp4"
+    , "content/acid/boiling-acid-4.mp4"
+    , "content/acid/boiling-acid-5.mp4"
+    , "content/acid/boiling-acid-6.mp4"
+];
+
+var TV = function ( video_selector, interstitial_selector ) {
+
+    var _screen = $( video_selector );
+    var _interstitial = $( interstitial_selector );
+    var _self = this;
+    var _next_interstitial = false;
+    var _interstitial_start_time;
+    var _clip_pos = 0;
+
+    var _clips_shuffled = [];
+
+    /**
+     *
+     * Based on:
+     * HTML5 video stretch
+     * http://coding.vdhdesign.co.nz/?p=29
+     * The video tag usually enforces the aspect ratio, but... CSS transforms to the rescue!
+     *
+     * @constructor
+     */
+    this.resizeVideo = function ( element ) {
+        //console.log( element );
+        var iOriginalVideoHeight = element.videoHeight;
+        var iCurrentVideoHeight = _screen.height();
+        var iVideoContainerHeight = $( element ).parent().height();
+        var iCurrentScale = iOriginalVideoHeight / iCurrentVideoHeight;
+        var iScaleY = (iVideoContainerHeight / iOriginalVideoHeight) * iCurrentScale;
+
+//        console.log( "iOriginalVideoHeight", iOriginalVideoHeight );
+//        console.log( "iCurrentVideoHeight", iCurrentVideoHeight );
+//        console.log( "iVideoContainerHeight", iVideoContainerHeight );
+//        console.log( "iCurrentScale", iCurrentScale );
+//
+//        console.log( "iScaleY ", iScaleY );
+//        console.log( _screen );
+
+        //Important to note: Set the origin to the top left corner (0% 0%), or else the position of the video goes astray
+        $( element ).css( {
+                              "transform-origin":         "0% 0%",
+                              "transform": "scaleY(" + iScaleY + ")",
+                              "-ms-transform-origin":     "0% 0% ", /* IE 9 */
+                              "-ms-transform": "scaleY(" + iScaleY + ")", /* IE 9 */
+                              "-moz-transform-origin":    "0% 0%", /* Firefox */
+                              "-moz-transform": "scaleY(" + iScaleY + ")", /* Firefox */
+                              "-webkit-transform-origin": "0% 0%", /* Safari and Chrome */
+                              "-webkit-transform": "scaleY(" + iScaleY + ")", /* Safari and Chrome */
+                              "-o-transform-origin":      "0% 0%", /* Opera */
+                              "-o-transform": "scaleY(" + iScaleY + ")" /* Opera */
+                          } );
+    }
+
+    /**
+     *
+     * @param video_src
+     */
+    function playVideo( video_src ) {
+        _screen[0].src = video_src;
+    }
+
+    /**
+     *
+     */
+    function loadNextVideo() {
+
+        if ( (new Date().getTime() - _interstitial_start_time) < 750 ) {
+            setTimeout( loadNextVideo, 100 );
+            return;
+        }
+
+        var clip_num = Math.round( Math.random() * (clip_list.length - 1) );
+
+        _clip_pos++;
+
+        playVideo( clip_list[ _clip_pos % clip_list.length ] );
+        _next_interstitial = true;
+        _self.resizeVideo( _interstitial[0] );
+    }
+
+    /**
+     *
+     */
+    function playInterstitial() {
+        _next_interstitial = false;
+        _screen[0].pause();
+        _interstitial[0].currentTime = 0;
+        _interstitial[0].play();
+        _interstitial.show();
+        _interstitial_start_time = new Date().getTime();
+        loadNextVideo();
+    }
+
+    /**
+     *
+     */
+    this.onClipLoaded = function () {
+        _interstitial[0].pause();
+        _interstitial.hide();
+        this.resizeVideo( _screen[0] );
+    }
+
+    /**
+     * When we're playing an interstitial, this will
+     */
+    this.onClipFinished = function () {
+        if ( _next_interstitial == true ) {
+            playInterstitial();
+        }
+        else {
+            loadNextVideo();
+        }
+
+    };
+
+    /**
+     * Shuffle an array
+     * http://stackoverflow.com/a/6274381/14615
+     * @param o
+     * @returns {*}
+     */
+    function shuffle( o ) {
+        for ( var j, x, i = o.length; i; j = Math.floor( Math.random() * i ), x = o[--i], o[i] = o[j], o[j] = x ) {
+            ;
+        }
+        return o;
+    }
+
+    function buildPlaylist() {
+        shuffle( clip_list );
+    }
+
+    buildPlaylist();
+    loadNextVideo();
+}
+
+/************************************************************
+ * Main
+ ************************************************************/
+$( document ).ready( function () {
+    tv = new TV( "video.screen", "video.interstitial" );
+    $( ".overlay" ).click( function () {
+        tv.onClipFinished();
+    } );
+} );
